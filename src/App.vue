a2b3c032a1f3a523114507589ab722b3961dc82f
monteokyla <monteokyla@gmail.com>
2023-10-20 01:59:22 +0800

    try langs

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
-<template></template>
+<script setup>
+import { ref } from 'vue'
+
+const course =ref('')
+
+</script>
+<template>
+
+<select v-model="course">
+    <option value="BSIT">BSIT</option>
+    <option value="BSCS">BSCS</option>
+    <option value="BSIS">BSIS</option>
+</select>
+<p>Course: {{course}} </p>
+
+<p v-if="course== 'BSIT'">
+<h1>Bachelor of Science Major in I miss you :3 </h1>
+</p>
+
+<p v-else> Relapse ka malala </p>
+
+
+
+</template>
 
 
